605587b4c9f2f681d0b5e58ea289b09d884e2923
Jose Rotchen <jose.rotchen14@gmail.com>
2025-03-18 10:38:01 -0300

    agregar nuevos logos y actualizar la descripción de experiencia en el layout

--- src/app/[lang]/(home)/components/technoligies.tsx
+++ src/app/[lang]/(home)/components/technoligies.tsx
@@ -1,92 +1,110 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleNodes } from "@fortawesome/free-solid-svg-icons";
 import { Badge } from "@/components/ui/badge";
+import Image from "next/image";
 
 const technologiesList = [
   {
     name: "Tyepscript",
     color: "#358ef1",
     text: "#fff",
+    logo: "typescript.svg",
   },
   {
     name: "Javascript",
     color: "#F0DB4F",
     text: "#323330",
+    logo: "javascript.svg",
   },
   {
     name: "PHP",
     color: "#484C89",
     text: "#000000",
+    logo: "php.svg",
   },
   {
     name: "Python",
     color: "#ffde57",
     text: "#4584b6",
+    logo: "python.svg",
   },
   {
     name: "Node.js",
     color: "#3c873a",
     text: "#303030",
+    logo: "node.svg",
   },
   {
     name: "React.js",
     color: "#00d8ff",
     text: "#fff",
+    logo: "react.png",
   },
   {
     name: "Next.js",
     color: "#000",
     text: "#fff",
+    logo: "next.svg",
+  },
+  {
+    name: "Angular.js",
+    color: "#dd1b16",
+    text: "#fff",
+    logo: "angular.png",
   },
   {
     name: "Nest.js",
     color: "#171A1C",
     text: "#E92747",
+    logo: "nest.svg",
   },
   {
     name: "Laravel",
     color: "#6c6c6c",
     text: "#f05340",
+    logo: "laravel.png",
   },
   {
     name: "TailwindCSS",
     color: "#06b6d4",
     text: "#fff",
+    logo: "tailwind.svg",
   },
   {
     name: "Sass",
     color: "#c69",
     text: "#fff",
+    logo: "sass.svg",
   },
   {
     name: "MongoDB",
     color: "#589636",
     text: "#fff",
+    logo: "mongodb.png",
   },
   {
     name: "MySQL",
     color: "#00758F",
     text: "#F29111",
+    logo: "mysql.svg",
   },
   {
     name: "PostgreSQL",
     color: "#0064a5",
     text: "#fff",
-  },
-  {
-    name: "Contentful",
-    color: "#3ab2e6",
-    text: "#fff",
+    logo: "postgresql.png",
   },
   {
     name: "Firebase",
     color: "#FF9100",
     text: "#fff",
+    logo: "firebase.svg",
   },
   {
     name: "Supabase",
     color: "#3ecf8e",
     text: "#000",
+    logo: "supabase.svg",
   },
 ];
 
@@ -133,13 +151,21 @@ export default async function Technologies({ lang }: Props) {
           {technologiesList.map((technology) => {
             return (
               <Badge
-                style={{
-                  backgroundColor: technology.color,
-                  color: technology.text,
-                }}
-                className={`md:text-base font-bold hover:scale-105 transition-all duration-300 ease-in-out`}
+                // style={{
+                //   backgroundColor: technology.color,
+                //   color: technology.text,
+                // }}
+                variant={"secondary"}
+                className={`md:text-base font-bold hover:scale-105 transition-all duration-300 ease-in-out flex items-center gap-2`}
                 key={technology.name}
               >
+                <Image
+                  src={`/logos/${technology.logo}`}
+                  alt={technology.name}
+                  width={35}
+                  height={35}
+                  className="w-9 h-auto"
+                />
                 {technology.name}
               </Badge>
             );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ const font = Nunito({
 export const metadata: Metadata = {
   title: "Jose Rotchen - Desarrollador Web",
   description:
-    "Full-stack especializado en Frontend con 2 años de experiencia. Mi pasión es crear aplicaciones centrándome en la usabilidad, el rendimiento y la experiencia del usuario (UX). Tengo muchas ganas de seguir creciendo y aprendiendo cada día, y mi sueño es poder crear aplicaciones que mejoren la vida de miles de personas. Posadas, Misiones - Argentina",
+    "Full-stack especializado en Frontend con 3 años de experiencia. Mi pasión es crear aplicaciones centrándome en la usabilidad, el rendimiento y la experiencia del usuario (UX). Tengo muchas ganas de seguir creciendo y aprendiendo cada día, y mi sueño es poder crear aplicaciones que mejoren la vida de miles de personas. Posadas, Misiones - Argentina",
 };
 
 export default function RootLayout({
@@ -26,11 +26,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="es">
-      <body className={font.className}>
-        <ThemeProvider attribute="class" disableTransitionOnChange>
-          {children}
-        </ThemeProvider>
+    <html lang="es" suppressHydrationWarning>
+      <body className={`${font.className} `}>
+        <ThemeProvider attribute="class">{children}</ThemeProvider>
       </body>
       {process.env.GOOGLE_ANALYTICS_ID && (
         <GoogleAnalytics gaId={process.env.GOOGLE_ANALYTICS_ID} />
